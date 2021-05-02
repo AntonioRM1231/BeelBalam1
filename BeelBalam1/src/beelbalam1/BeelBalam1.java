a9dfd0e0d2f3df8360a9454556da5d726bb57379
AntonioRM1231 <rodriguezantony1231@gmail.com>
2021-05-01 20:07:48 -0500

    Agregue una nueva variable

--- BeelBalam1/src/beelbalam1/BeelBalam1.java
+++ BeelBalam1/src/beelbalam1/BeelBalam1.java
@@ -15,6 +15,7 @@ public class BeelBalam1 {
      * @param args the command line arguments
      */
    	public int numero = 0;
+        public int numero2 =9;
     public static void main(String[] args) {
         // TODO code application logic here
     }

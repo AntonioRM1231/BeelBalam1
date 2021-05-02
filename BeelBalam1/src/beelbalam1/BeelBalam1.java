f77a2ce090d1a22e95a55cd5feb4df0fb8eaa74e
Anahi Salto L <anahisalto16@gmail.com>
2021-05-01 20:18:15 -0500

    Reiniciando todo

--- BeelBalam1/src/beelbalam1/BeelBalam1.java
+++ BeelBalam1/src/beelbalam1/BeelBalam1.java
@@ -14,11 +14,7 @@ public class BeelBalam1 {
     /**
      * @param args the command line arguments
      */
-<<<<<<< HEAD
-=======
-   	public int numero = 0;
-        public int numero2 =9;
->>>>>>> a9dfd0e0d2f3df8360a9454556da5d726bb57379
+   	
     public static void main(String[] args) {
         // TODO code application logic here
     }
